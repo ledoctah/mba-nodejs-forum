f64e3f19ab8d6cb0ac6427622aa30de4a92cbe86
Matheus <matheus@matheusferreira.dev>
2024-11-09 13:16:41 -0300

    refactor: refactored entities

--- src/domain/entities/question.ts
+++ src/domain/entities/question.ts
@@ -1,4 +1,7 @@
-import { randomUUID } from "node:crypto";
+import dayjs from "dayjs";
+import { Entity } from "../../core/entities/entity";
+import { UniqueEntityID } from "../../core/entities/unique-entity-id";
+import { Optional } from "../../core/types/optional";
 
 import { Slug } from "./value-objects/slug";
 
@@ -6,21 +9,82 @@ interface QuestionProps {
   title: string;
   content: string;
   slug: Slug;
-  authorId: string;
+  authorId: UniqueEntityID;
+  bestAnswerId?: UniqueEntityID;
+  createdAt: Date;
+  updatedAt?: Date;
 }
 
-class Question {
-  public id: string;
-  public title: string;
-  public slug: Slug;
-  public content: string;
-  public authorId: string;
+export class Question extends Entity<QuestionProps> {
+  get title() {
+    return this.props.title;
+  }
 
-  constructor(props: QuestionProps, id?: string) {
-    this.title = props.title;
-    this.slug = props.slug;
-    this.content = props.content;
-    this.authorId = props.authorId;
-    this.id = id ?? randomUUID();
+  set title(title: string) {
+    this.props.title = title;
+    this.props.slug = Slug.createFromText(title);
+    this.touch();
+  }
+
+  get slug() {
+    return this.props.slug;
+  }
+
+  get content() {
+    return this.props.content;
+  }
+
+  set content(content: string) {
+    this.props.content = content;
+    this.touch();
+  }
+
+  get authorId() {
+    return this.props.authorId;
+  }
+
+  get bestAnswerId() {
+    return this.props.bestAnswerId;
+  }
+
+  set bestAnswerId(bestAnswerId: UniqueEntityID | undefined) {
+    this.props.bestAnswerId = bestAnswerId;
+    this.touch();
+  }
+
+  get createdAt() {
+    return this.props.createdAt;
+  }
+
+  get updatedAt() {
+    return this.props.updatedAt;
+  }
+
+  get isNew(): boolean {
+    return dayjs().diff(this.createdAt, 'days') <= 3;
+  }
+  
+  get excerpt() {
+    return this.content
+      .substring(0, 120)
+      .trimEnd()
+      .concat('...');
+  }
+
+  private touch() {
+    this.props.updatedAt = new Date();
+  }
+
+  static create(
+    props: Optional<QuestionProps, 'createdAt' | 'slug'>,
+    id?: UniqueEntityID,
+  ) {
+    const question = new Question({
+      ...props,
+      slug: props.slug ?? Slug.createFromText(props.title),
+      createdAt: new Date()
+    }, id);
+
+    return question;
   }
 }
--- src/domain/use-cases/answer-question.use-case.ts
+++ src/domain/use-cases/answer-question.use-case.ts
@@ -1,3 +1,4 @@
+import { UniqueEntityID } from "../../core/entities/unique-entity-id";
 import { Answer } from "../entities/answer";
 import { AnswersRepository } from "../repositories/answers-repository";
 
@@ -13,10 +14,10 @@ export class AnswerQuestionUseCase {
   ) {}
 
   async execute({ instructorId, questionId, content }: AnswerQuestionUseCaseRequest) {
-    const answer = new Answer({
+    const answer = Answer.create({
       content,
-      questionId,
-      authorId: instructorId,
+      questionId: new UniqueEntityID(questionId),
+      authorId: new UniqueEntityID(instructorId),
     });
 
     await this.answersRepository.create(answer);
