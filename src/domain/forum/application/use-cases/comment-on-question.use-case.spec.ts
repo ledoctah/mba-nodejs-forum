a1e9eddad078a95b3a9c99401d03e678a776f906
Matheus <matheus@matheusferreira.dev>
2024-11-09 19:28:01 -0300

    feat: add comment on question use case

---
+++ src/domain/forum/application/use-cases/comment-on-question.use-case.spec.ts
@@ -0,0 +1,37 @@
+import { makeQuestion } from 'test/factories/make-question';
+import { InMemoryQuestionCommentsRepository } from 'test/repositories/in-memory-questions-comments-repository';
+import { InMemoryQuestionsRepository } from 'test/repositories/in-memory-questions-repository';
+
+import { CommentOnQuestionUseCase } from './comment-on-question.use-case';
+
+let inMemoryQuestionsRepository: InMemoryQuestionsRepository;
+let inMemoryQuestionCommentsRepository: InMemoryQuestionCommentsRepository;
+let sut: CommentOnQuestionUseCase;
+
+describe('Comment On Question', () => {
+  beforeEach(() => {
+    inMemoryQuestionsRepository = new InMemoryQuestionsRepository();
+    inMemoryQuestionCommentsRepository =
+      new InMemoryQuestionCommentsRepository();
+    sut = new CommentOnQuestionUseCase(
+      inMemoryQuestionsRepository,
+      inMemoryQuestionCommentsRepository,
+    );
+  });
+
+  it('should be able to comment on question', async () => {
+    const question = makeQuestion();
+
+    await inMemoryQuestionsRepository.create(question);
+
+    await sut.execute({
+      questionId: question.id.toString(),
+      authorId: question.authorId.toString(),
+      content: 'Test comment',
+    });
+
+    expect(inMemoryQuestionCommentsRepository.items[0].content).toEqual(
+      'Test comment',
+    );
+  });
+});
